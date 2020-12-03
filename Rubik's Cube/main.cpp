371e4f56b22339b2bf8501c4bbb2a542914d6224
unknown <newjincar33@gmail.com>
2020-12-03 17:37:23 +0900

    큐브 자동 섞기, 사용자 입력 처리 추가

--- Rubik's Cube/main.cpp
+++ Rubik's Cube/main.cpp
@@ -10,14 +10,126 @@ private:
 	Rubiks rubiksCube;
 	Input inputManager;
 
+	vector<string> GetCommandList() {
+		vector<string> commandList;
+
+		if (315 < thetaY || thetaY <= 45) {
+			if (thetaX <= -45) {
+				commandList = {
+					"i1+", "k1+", "k1-", "i1-",
+					"k2+", "k2-", "i0+", "i0-",
+					"i2+", "i2-", "k0+", "k0-",
+				};
+			}
+			else if (thetaX > 45) {
+				commandList = {
+					"i1+", "k1-", "k1+", "i1-",
+					"k0-", "k0+", "i0+", "i0-",
+					"i2+", "i2-", "k2-", "k2+",
+				};
+			}
+			else {
+				commandList = { 
+					"i1+", "j1+", "j1-", "i1-", 
+					"j2+", "j2-", "i0+", "i0-", 
+					"i2+", "i2-", "j0+", "j0-",
+				};
+			}
+		}
+		else if (45 < thetaY && thetaY <= 135) {
+			if (thetaX <= -45) {
+				commandList = {
+					"k1+", "i1-", "i1+", "k1-",
+					"i0-", "i0+", "k0+", "k0-",
+					"k2+", "k2-", "i2-", "i2+",
+				};
+			}
+			else if (thetaX > 45) {
+				commandList = {
+					"k1+", "i1+", "i1-", "k1-",
+					"i2+", "i2-", "k0+", "k0-",
+					"k2+", "k2-", "i0+", "i0-",
+				};
+			}
+			else {
+				commandList = {
+					"k1+", "j1+", "j1-", "k1-",
+					"j2+", "j2-", "k0+", "k0-",
+					"k2+", "k2-", "j0+", "j0-",
+				};
+			}
+		}
+		else if (135 < thetaY && thetaY <= 225) {
+			if (thetaX <= -45) {
+				commandList = {
+					"i1-", "k1-", "k1+", "i1+",
+					"k0-", "k0+", "i2-", "i2+",
+					"i0-", "i0+", "k2-", "k2+",
+				};
+			}
+			else if (thetaX > 45) {
+				commandList = {
+					"i1-", "k1+", "k1-", "i1+",
+					"k2+", "k2-", "i2-", "i2+",
+					"i0-", "i0+", "k0+", "k0-",
+				};
+			}
+			else {
+				commandList = {
+					"i1-", "j1+", "j1-", "i1+",
+					"j2+", "j2-", "i2-", "i2+",
+					"i0-", "i0+", "j0+", "j0-",
+				};
+			}
+		}
+		else if (225 < thetaY && thetaY <= 315) {
+			if (thetaX <= -45) {
+				commandList = {
+					"k1-", "i1+", "i1-", "k1+",
+					"i2+", "i2-", "k2-", "k2+",
+					"k0-", "k0+", "i0+", "i0-",
+				};
+			}
+			else if (thetaX > 45) {
+				commandList = {
+					"k1-", "i1-", "i1+", "k1+",
+					"i0-", "i0+", "k2-", "k2+",
+					"k0-", "k0+", "i2-", "i2+",
+				};
+			}
+			else {
+				commandList = {
+					"k1-", "j1+", "j1-", "k1+",
+					"j2+", "j2-", "k2-", "k2+",
+					"k0-", "k0+", "j0+", "j0-",
+				};
+			}
+		}
+
+		return commandList;
+	}
+
 public:
 	RubiksEngine(short width, short height) : Engine(width, height) {
-		thetaX = 0;
-		thetaY = 0;
+		thetaX = 45;
+		thetaY = 45;
 	}
 
 	void OnCreate() override {
+		srand(GetTickCount64());
 
+		const char* symbols = "ijk";
+		const char* indexes = "012";
+		const char* directions = "+-";
+
+		for (int i = 0; i < 20; ++i) {
+			string command = "";
+			command += symbols[rand() % 3];
+			command += indexes[rand() % 3];
+			command += directions[rand() % 2];
+
+			rubiksCube.Control(command);
+		}
 	}
 
 	void OnUpdate(double deltaTime) override {
@@ -26,56 +138,58 @@ public:
 		//유저 입력 처리
 		inputManager.UpdateKeyStates();
 
-		if (inputManager.GetKeyState('W') >= KeyPressed) {
-			thetaX -= deltaTime * 90;
-			thetaX = MAX(thetaX, -60);
-		}
-		if (inputManager.GetKeyState('A') >= KeyPressed) {
-			thetaY += deltaTime * 90;
-			if (thetaY >= 360) thetaY -= 360;
-		}
-		if (inputManager.GetKeyState('S') >= KeyPressed) {
-			thetaX += deltaTime * 90;
+		if (inputManager.GetKeyState(VK_UP) >= KeyPressed) {
+			thetaX += deltaTime * 120;
 			thetaX = MIN(thetaX, +60);
 		}
-		if (inputManager.GetKeyState('D') >= KeyPressed) {
-			thetaY -= deltaTime * 90;
+		if (inputManager.GetKeyState(VK_DOWN) >= KeyPressed) {
+			thetaX -= deltaTime * 120;
+			thetaX = MAX(thetaX, -60);
+		}
+		if (inputManager.GetKeyState(VK_LEFT) >= KeyPressed) {
+			thetaY -= deltaTime * 120;
 			if (thetaY <= 0) thetaY += 360;
 		}
+		if (inputManager.GetKeyState(VK_RIGHT) >= KeyPressed) {
+			thetaY += deltaTime * 120;
+			if (thetaY >= 360) thetaY -= 360;
+		}
+
+		vector<string> commandList = GetCommandList();
 
 		if (inputManager.GetKeyState(VK_NUMPAD5) == KeyPressed) {
 			if (inputManager.GetKeyState(VK_NUMPAD2) == KeyDown)
-				rubiksCube.Control("i1+");
+				rubiksCube.Control(commandList[0]);
 			else if (inputManager.GetKeyState(VK_NUMPAD4) == KeyDown)
-				rubiksCube.Control("j1+");
+				rubiksCube.Control(commandList[1]);
 			else if (inputManager.GetKeyState(VK_NUMPAD6) == KeyDown)
-				rubiksCube.Control("j1-");
+				rubiksCube.Control(commandList[2]);
 			else if (inputManager.GetKeyState(VK_NUMPAD8) == KeyDown)
-				rubiksCube.Control("i1-");
+				rubiksCube.Control(commandList[3]);
 		}
 		else if (inputManager.GetKeyState(VK_NUMPAD2) == KeyPressed) {
 			if (inputManager.GetKeyState(VK_NUMPAD1) == KeyDown)
-				rubiksCube.Control("j2+");
+				rubiksCube.Control(commandList[4]);
 			else if (inputManager.GetKeyState(VK_NUMPAD3) == KeyDown)
-				rubiksCube.Control("j2-");
+				rubiksCube.Control(commandList[5]);
 		}
 		else if (inputManager.GetKeyState(VK_NUMPAD4) == KeyPressed) {
 			if (inputManager.GetKeyState(VK_NUMPAD1) == KeyDown)
-				rubiksCube.Control("i0+");
+				rubiksCube.Control(commandList[6]);
 			else if (inputManager.GetKeyState(VK_NUMPAD7) == KeyDown)
-				rubiksCube.Control("i0-");
+				rubiksCube.Control(commandList[7]);
 		}
 		else if (inputManager.GetKeyState(VK_NUMPAD6) == KeyPressed) {
 			if (inputManager.GetKeyState(VK_NUMPAD3) == KeyDown)
-				rubiksCube.Control("i2+");
+				rubiksCube.Control(commandList[8]);
 			else if (inputManager.GetKeyState(VK_NUMPAD9) == KeyDown)
-				rubiksCube.Control("i2-");
+				rubiksCube.Control(commandList[9]);
 		}
 		else if (inputManager.GetKeyState(VK_NUMPAD8) == KeyPressed) {
 			if (inputManager.GetKeyState(VK_NUMPAD7) == KeyDown)
-				rubiksCube.Control("j0+");
+				rubiksCube.Control(commandList[10]);
 			else if (inputManager.GetKeyState(VK_NUMPAD9) == KeyDown)
-				rubiksCube.Control("j0-");
+				rubiksCube.Control(commandList[11]);
 		}
 
 		//큐브 렌더링
